22380128bc869504a086df739b803a556e37de0e
1073650251@qq.com <1073650251@qq.com>
2019-06-26 15:35:55 +0800

    修改了webpack配并且可以正确打包文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,90 @@
+const webpack = require('webpack');
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'index_bundle.js'
+    entry: {
+        bundle: path.join(__dirname, './src/index'),
+        vendor: ['react', 'react-dom', 'react-router', 'react-router-dom', 'react-redux']
     },
-    devtool:false,
-    // resolve: {
-    //     extensions: ['.js', '.jsx', '.css', '.scss', '.json'],
-    //     alias: {
-    //         //配置后可以直接用@来表示这个位置,直接应用目录下得文件,是绝对路径
-    //         '@': path.join(__dirname, './src')
-    //     }
-    // },
-
-    // module: {
-    //     rules: [
-    //         {
-    //             //自動編譯 JSX 或 JS 檔 （require 可載入 JSX 了）
-    //             test: /\.(js|jsx)$/,
-    //             use: { loader: 'babel-loader' },
-    //             exclude: /node_modules/
-    //         }
-    //     ]
-    // },
-
-
+    output: {
+        filename: 'index.js',
+        path: __dirname + '/dist'
+    },
     module: {
-        loaders: [
+        rules: [ //配置加载器
             {
-                test: /\.js|jsx$/,
-                loader: 'babel-loader',
-                query: {
-                    presets:['es2015','react','stage-0']
+                test: /\.js$/, //配置要处理的文件格式，一般使用正则表达式匹配
+                loader: 'babel-loader', //使用的加载器名称
+                query: { //babel的配置参数，可以写在.babelrc文件里也可以写在这里
+                    presets: ["@babel/env", "@babel/react"],
+                    "plugins": [
+                        "dynamic-import-webpack",
+                        "@babel/plugin-proposal-class-properties"
+                    ]
                 }
             },
             {
-                test:/\.css$/,
-                loaders:['style','css']
+                test: /\.css$/,
+                // loader: ExtractTextPlugin.extract('style-loader!css-loader'), //使用的加载器名称
+                // loader: 'style-loader!css-loader'
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            },
+            {   
+                test:[/\.eot$/,/\.svg$/,/\.ttf$/,/\.woff$/,/\.woff2$/],
+                loader:['file-loader']
             },
             {
-                test:/\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url',
-                query:{
-                    limit: 10000,
-                    name:'./build/img/[name].[hash:7].[ext]'
-                }
-            },
-            {
-                test:/\.(woff2?|eot|ttf|otf)(\?.*)?&/,
-                loader:'url',
-                query: {
-                    limit:10000,
-                    name:'./build/fonts/[name].[hash:7].[ext]'
-                }
+                test: [/\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/],
+                loader: 'url-loader',
+                options: {
+                  limit: 10000, //1w字节以下大小的图片会自动转成base64
+                },
             }
         ]
     },
-    plugins:[
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', //指定模板路径
+            filename: 'index.html', //指定文件名
+        }),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            output: {
-                comments: false
+                NODE_ENV: JSON.stringify('production'),
             },
-            compress: {
-                warnings: false
-            }
         }),
-        new webpack.optimize.AggressiveMergingPlugin()
-    ]
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+        
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJSPlugin({
+                uglifyOptions: {
+                    output: {
+                        comments: false
+                    },
+                    compress: {
+                        drop_console: true
+                    }
+                }
+            }),
+        ],
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    name: "vendor",
+                    chunks: "initial",
+                    minChunks: 2
+                }
+            }
+        }
+    }
 }
